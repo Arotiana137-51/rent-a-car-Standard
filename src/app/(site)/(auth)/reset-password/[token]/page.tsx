424c120d0272fe574720be03c0a90ea6955950f8
Arotiana137-51 <arotiana4612@gmail.com>
2024-10-14 10:17:09 +0300

    change name

--- src/app/(site)/(auth)/reset-password/[token]/page.tsx
+++ src/app/(site)/(auth)/reset-password/[token]/page.tsx
@@ -4,7 +4,7 @@ import Breadcrumb from "@/components/Common/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Reset Password | Play SaaS Starter Kit and Boilerplate for Next.js",
+  title: "Reset Password | Car rental services in Madagascar",
 };
 
 const ResetPasswordPage = ({ params }: { params: { token: string } }) => {
